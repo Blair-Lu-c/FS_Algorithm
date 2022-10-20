56bd9998894ed6968457f00450dbfd02e0a42ee7
root <root@Blair.localdomain>
2022-10-20 16:45:46 +0200

    last version

--- Assignments/Integer_to_Roman.py
+++ Assignments/Integer_to_Roman.py
@@ -1,20 +1,24 @@
-class Solution(object):
+class integer_to_roman:
     def intToRoman(self, num):
+        """
+        Function to convert decimal to Roman Numerals
+        """
         a = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
         b = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
         res = ''
-        if num <= 0 or num > 39999:
+        if num <= 0 or num > 3999:
             print()
         for i, n in enumerate(a):
-            while num>=a[i]:
-                res+=b[i]
-                num-=a[i]
+            while num >= a[i]:
+                res += b[i]
+                num -= a[i]
         return res
 
+# Diver Code
 if __name__ == '__main__':
     num = int(input("Enter integer: "))
     if num <= 0 or num > 3999:
         print('Your input integer should be range from 0 to 3999!')
     else:
-        ob1 = Solution()
-        print(f'Integer to Roman: {ob1.intToRoman(num)}')
+        roman = integer_to_roman()
+        print(f'Integer to Roman: {roman.intToRoman(num)}')
